98dc2ea053cd2458320fc7ad1cf19fa69b112acc
jefbiel <143344048+jefbiel@users.noreply.github.com>
2025-08-18 11:31:36 -0300

    Update Program.cs

--- StartC/Program.cs
+++ StartC/Program.cs
@@ -6,7 +6,33 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string aluno1, aluno2;
+            int age1, age2;
+
+            Console.WriteLine("Insira o nome do primeiro aluno:");
+            aluno1 = Console.ReadLine();
+
+            Console.WriteLine("Insira a idade do primeiro aluno:");
+            age1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o nome do segundo aluno:");
+            aluno2 = Console.ReadLine();
+
+            Console.WriteLine("Insira a idade do segundo aluno:");
+            age2 = int.Parse(Console.ReadLine());
+
+            if (age1 == age2)
+            {
+                Console.WriteLine("As idades são iguais.");
+            }
+            if (age1 > age2)
+            {
+                Console.WriteLine($"{aluno1} é mais velho que {aluno2}.");
+            }
+            if (age1 < age2)
+            {
+                Console.WriteLine($"{aluno2} é mais velho que {aluno1}.");
+            }
         }
     }
 }
